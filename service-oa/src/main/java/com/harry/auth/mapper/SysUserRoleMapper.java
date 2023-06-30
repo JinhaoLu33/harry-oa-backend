46ba1c90d734f258e2b0b2aef4bd9a1162c965d3
Jinhao <u7531876@anu.edu.au>
2023-06-30 11:19:31 +0800

    user manage api

---
+++ service-oa/src/main/java/com/harry/auth/mapper/SysUserRoleMapper.java
@@ -0,0 +1,16 @@
+package com.harry.auth.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.harry.model.system.SysUserRole;
+
+/**
+ * <p>
+ * 用户角色 Mapper 接口
+ * </p>
+ *
+ * @author Harry
+ * @since 2023-06-29
+ */
+public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
+
+}
---
+++ service-oa/src/main/java/com/harry/auth/service/impl/SysUserRoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.harry.auth.service.impl;
+
+
+import com.harry.auth.mapper.SysUserRoleMapper;
+import com.harry.auth.service.SysUserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.harry.model.system.SysUserRole;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户角色 服务实现类
+ * </p>
+ *
+ * @author Harry
+ * @since 2023-06-29
+ */
+@Service
+public class SysUserRoleServiceImpl extends ServiceImpl<SysUserRoleMapper, SysUserRole> implements SysUserRoleService {
+
+}
